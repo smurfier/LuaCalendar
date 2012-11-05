ad44582007af384c0d40a162e5b432015e112d6b
John Murphy <smurfier20@gmail.com>
2012-11-05 14:14:18 -0600

    Moved main variables to matrix

--- @Resources/CScript.lua
+++ @Resources/CScript.lua
@@ -11,23 +11,27 @@ function Initialize()
 		LText = SELF:GetOption('LabelText', '{MName}, {Year}'),
 		NFormat = SELF:GetOption('NextFormat', '{day}: {desc}'):lower(),
 	}
-
-	Time, mLength, StartDay, Month, Year, InMonth, Old = {}, 0, 0, 0, 0, true, {Day = 0, Month = 0, Year = 0}
+	Time = {
+		curr = {},
+		old = {day = 0, month = 0, year = 0,},
+		show = {month = 0, year = 0,},
+		stats = {clength = 0, plength = 0, startday= 0, inmonth = true,}
+	}
 
 	local sRange = SELF:GetOption('Range', 'month'):lower():gsub(' ', '')
 	if not ('week|month'):find(sRange) then ErrMsg(nil, 'Invalid Range: %s', sRange) end
 	if sRange == 'week' then
 		Range = {
-			formula = function(input) return Time.day +((input - 1) - rotate(Time.wday - 1)) end,
+			formula = function(input) return Time.curr.day +((input - 1) - rotate(Time.curr.wday - 1)) end,
 			days = 7,
 			week = function() return 1 end,
 			nomove = true,
 		}
 	else
 		Range = {
-			formula = function(input) return input - StartDay end,
+			formula = function(input) return input - Time.stats.startday end,
 			days = 42,
-			week = function() return math.ceil((Time.day + StartDay) / 7) end,
+			week = function() return math.ceil((Time.curr.day + Time.stats.startday) / 7) end,
 		}
 	end
 	-- Weekday labels text
@@ -47,22 +51,22 @@ function Initialize()
 end -- Initialize
 
 function Update()
-	Time = os.date('*t')
+	Time.curr = os.date('*t')
 	
 	-- If in the current month or if browsing and Month changes to that month, set to Real Time
-	if (InMonth and Month ~= Time.month) or ((not InMonth) and Month == Time.month and Year == Time.year) then
+	if (Time.stats.inmonth and Time.show.month ~= Time.curr.month) or ((not Time.stats.inmonth) and Time.show.month == Time.curr.month and Time.show.year == Time.curr.year) then
 		Move()
 	end
 	
-	if Month ~= Old.Month or Year ~= Old.Year then -- Recalculate and Redraw if Month and/or Year changes
-		Old = {Month=Month, Year=Year, Day=Time.day}
-		local tstart = os.time{day = 1, month = Month, year = Year, isdst = false,}
-		local nstart = os.time{day = 1, month = (Month % 12 + 1), year = (Year + (Month == 12 and 1 or 0)), isdst = false,}
-		mLength, pLength, StartDay = (nstart - tstart) / 86400, tonumber(os.date('%d', tstart - 86400)), rotate(tonumber(os.date('%w', tstart)))
+	if Time.show.month ~= Time.old.month or Time.show.year ~= Time.old.year then -- Recalculate and Redraw if Month and/or Year changes
+		Time.old = {month=Time.show.month, year=Time.show.year, day=Time.curr.day}
+		local tstart = os.time{day = 1, month = Time.show.month, year = Time.show.year, isdst = false,}
+		local nstart = os.time{day = 1, month = (Time.show.month % 12 + 1), year = (Time.show.year + (Time.show.month == 12 and 1 or 0)), isdst = false,}
+		Time.stats = {clength = ((nstart - tstart) / 86400), plength = (tonumber(os.date('%d', tstart - 86400))), startday = rotate(tonumber(os.date('%w', tstart))),}
 		Events()
 		Draw()
-	elseif Time.day ~= Old.Day then -- Redraw if Today changes
-		Old.Day = Time.day
+	elseif Time.curr.day ~= Time.old.day then -- Redraw if Today changes
+		Time.old.day = Time.curr.day
 		Draw()
 	end
 	
@@ -163,7 +167,7 @@ function Events() -- Parse Events table.
 	Hol = setmetatable({}, { __call = function(self) -- Returns a list of events
 		local Evns = {}
 	
-		for day = InMonth and Time.day or 1, mLength do -- Parse through month days to keep days in order
+		for day = Time.stats.inmonth and Time.curr.day or 1, Time.stats.clength do -- Parse through month days to keep days in order
 			if self[day] then
 				local tbl = setmetatable({day = day, desc = table.concat(self[day]['text'], ', ')},
 					{ __index = function(_, input) return ErrMsg('', 'Invalid NextFormat variable {%s}', input) end,})
@@ -188,7 +192,7 @@ function Events() -- Parse Events table.
 
 	for _, event in ipairs(hFile) do
 		local eMonth = formula(event.month, event.descri)
-		if  eMonth == Month or event['repeat'] then
+		if  eMonth == Time.show.month or event['repeat'] then
 			local day = formula(event.day, event.descri) or ErrMsg(0, 'Invalid Event Day %s in %s', event.day, event.descri)
 			local desc = event.descri .. '%s' .. (event.title and ' -' .. event.title or '')
 
@@ -197,32 +201,32 @@ function Events() -- Parse Events table.
 			if nrepeat == 'week' then
 				if eMonth and event.year and day then
 					local stamp = os.time{month = eMonth, day = day, year = event.year,}
-					local test = os.time{month = Month, day = day, year = Year,} >= stamp
-					local mstart = os.time{month = Month, day = 1, year = Year,}
+					local test = os.time{month = Time.show.month, day = day, year = Time.show.year,} >= stamp
+					local mstart = os.time{month = Time.show.month, day = 1, year = Time.show.year,}
 					local multi = event.multip * 604800
 					local first = mstart + ((stamp - mstart) % multi)
 
 					for a = 0, 4 do
 						local tstamp = first + a * multi
 						local temp = os.date('*t', tstamp)
-						if temp.month == Month and test then
+						if temp.month == Time.show.month and test then
 							AddEvn(temp.day, desc, event.color, event.annive and ((tstamp - stamp) / multi + 1) or false)
 						end
 					end
 				end
 			elseif nrepeat == 'year' then
-				local test = (event.year and event.multip > 1) and ((Year - event.year) % event.multip) or 0
+				local test = (event.year and event.multip > 1) and ((Time.show.year - event.year) % event.multip) or 0
 
 				if eMonth == Month and test == 0 then
-					AddEvn(day, desc, event.color, event.annive and (Year - event.year / event.multip) or false)
+					AddEvn(day, desc, event.color, event.annive and (Time.show.year - event.year / event.multip) or false)
 				end
 			elseif nrepeat == 'month' then
 				if eMonth and event.year then
 					if Year >= event.year then
-						local ydiff = Year - event.year - 1
-						local mdiff = ydiff == -1 and (Month - eMonth) or ((12 - eMonth) + Month + (ydiff * 12))
+						local ydiff = Time.show.year - event.year - 1
+						local mdiff = ydiff == -1 and (Time.show.month - eMonth) or ((12 - eMonth) + Time.show.month + (ydiff * 12))
 						local estamp = os.time{year = event.year, month = eMonth, day = 1,}
-						local mstart = os.time{year = Year, month = Month, day = 1,}
+						local mstart = os.time{year = Time.show.year, month = Time.show.month, day = 1,}
 
 						if (mdiff % event.multip) == 0 and mstart >= estamp then
 							AddEvn(day, desc, event.color, event.annive and (mdiff / event.multip + 1) or false)
@@ -231,7 +235,7 @@ function Events() -- Parse Events table.
 				else
 					AddEvn(day, desc, event.color, false)
 				end
-			elseif event.year == Year then
+			elseif event.year == Time.show.year then
 				AddEvn(day, desc, event.color)
 			end
 		end
@@ -239,14 +243,14 @@ function Events() -- Parse Events table.
 end -- Events
 
 function Draw() -- Sets all meter properties and calculates days
-	local LastWeek = Set.HLWeek and math.ceil((StartDay + mLength) / 7) < 6
+	local LastWeek = Set.HLWeek and math.ceil((Time.stats.startday + Time.stats.clength) / 7) < 6
 	
 	for wday = 1, 7 do -- Set Weekday Labels styles
 		local Styles = {'LblTxtSty'}
 		if wday == 1 then
 			table.insert(Styles, 'LblTxtStart')
 		end
-		if rotate(Time.wday - 1) == (wday - 1) and InMonth then
+		if rotate(Time.curr.wday - 1) == (wday - 1) and Time.stats.inmonth then
 			table.insert(Styles, 'LblCurrSty')
 		end
 		SKIN:Bang('!SetOption', Set.DPref .. wday, 'MeterStyle', table.concat(Styles, '|'))
@@ -261,7 +265,7 @@ function Draw() -- Sets all meter properties and calculates days
 			table.insert(Styles, 'NewWk')
 		end
 		-- Holiday ToolTip and Style
-		if day > 0 and day <= mLength and Hol[day] then
+		if day > 0 and day <= Time.stats.clength and Hol[day] then
 			event = table.concat(Hol[day].text, '\n')
 			table.insert(Styles, 'HolidayStyle')
 
@@ -277,15 +281,15 @@ function Draw() -- Sets all meter properties and calculates days
 			end
 		end
 		
-		if (Time.day + StartDay) == meter and InMonth then
+		if (Time.curr.day + Time.stats.startday) == meter and Time.stats.inmonth then
 			table.insert(Styles, 'CurrentDay')
 		elseif meter > 35 and LastWeek then
 			table.insert(Styles, 'LastWeek')
 		elseif day < 1 then
-			day = day + pLength
+			day = day + Time.stats.plength
 			table.insert(Styles, 'PreviousMonth')
-		elseif day > mLength then
-			day = day - mLength
+		elseif day > Time.stats.clength then
+			day = day - Time.stats.clength
 			table.insert(Styles, 'NextMonth')
 		elseif (meter % 7) == 0 or (meter % 7) == (Set.SMon and 6 or 1) then
 			table.insert(Styles, 'WeekendStyle')
@@ -301,16 +305,16 @@ function Draw() -- Sets all meter properties and calculates days
 	
 	for k, v in pairs{ -- Define skin variables
 		ThisWeek = Range.week(),
-		Week = rotate(Time.wday - 1),
-		Today = LZero(Time.day),
-		Month = MLabels[Month],
-		Year = Year,
+		Week = rotate(Time.curr.wday - 1),
+		Today = LZero(Time.curr.day),
+		Month = MLabels[Time.show.month],
+		Year = Time.show.year,
 		MonthLabel = Vars(Set.LText, 'MonthLabel'),
 		LastWkHidden = LastWeek and 1 or 0,
 		NextEvent = Hol(),
 	} do SKIN:Bang('!SetVariable', k, v) end
 	-- Week Numbers for the current month
-	local FirstWeek = os.time{day = 6 - StartDay, month = Month, year = Year}
+	local FirstWeek = os.time{day = 6 - Time.stats.startday, month = Time.show.month, year = Time.show.year}
 	for i = 0, 5 do
 		SKIN:Bang('!SetVariable', 'WeekNumber' .. (i + 1), math.ceil(tonumber(os.date('%j', (FirstWeek + (i * 604800)))) / 7))
 	end
@@ -318,40 +322,40 @@ end -- Draw
 
 function Move(value) -- Move calendar through the months
 	if Range.nomove or not value then
-		Month, Year = Time.month, Time.year
+		Time.show = Time.curr
 	elseif math.ceil(value) ~= value then
 		ErrMsg(nil, 'Invalid Move Parameter %s', value)
 	else
-		local mvalue = Month + value - (math.modf(value / 12)) * 12
+		local mvalue = Time.show.month + value - (math.modf(value / 12)) * 12
 		local mchange = value < 0 and (mvalue < 1 and 12 or 0) or (mvalue > 12 and -12 or 0)
-		Month, Year = (mvalue + mchange), (Year + (math.modf(value / 12)) - mchange / 12)
+		Time.show = {month = (mvalue + mchange), year = (Time.show.year + (math.modf(value / 12)) - mchange / 12),}
 	end
 
-	InMonth = Month == Time.month and Year == Time.year
-	SKIN:Bang('!SetVariable', 'NotCurrentMonth', InMonth and 0 or 1)
+	Time.stats.inmonth = Time.show.month == Time.curr.month and Time.show.year == Time.curr.year
+	SKIN:Bang('!SetVariable', 'NotCurrentMonth', Time.stats.inmonth and 0 or 1)
 end -- Move
 
 function Easter() -- Returns a timestamp representing easter of the current year
-	local a, b, c, h, L, m = (Year % 19), math.floor(Year / 100), (Year % 100), 0, 0, 0
+	local a, b, c, h, L, m = (Time.show.year % 19), math.floor(Time.show.year / 100), (Time.show.year % 100), 0, 0, 0
 	local d, e, f, i, k = math.floor(b/4), (b % 4), math.floor((b + 8) / 25), math.floor(c / 4), (c % 4)
 	h = (19 * a + b - d - math.floor((b - f + 1) / 3) + 15) % 30
 	L = (32 + 2 * e + 2 * i - h - k) % 7
 	m = math.floor((a + 11 * h + 22 * L) / 451)
 	
-	return os.time{month = math.floor((h + L - 7 * m + 114) / 31), day = ((h + L - 7 * m + 114) % 31 + 1), year = Year}
+	return os.time{month = math.floor((h + L - 7 * m + 114) / 31), day = ((h + L - 7 * m + 114) % 31 + 1), year = Time.show.year}
 end -- Easter
 
 function Vars(line, source) -- Makes allowance for {Variables}
-	local tbl = setmetatable({mname = MLabels[Month], year = Year, today = LZero(Time.day), month = Month},
+	local tbl = setmetatable({mname = MLabels[Time.show.month], year = Time.show.year, today = LZero(Time.curr.day), month = Time.show.month},
 		{ __index = function(_, input)
 			local D, W = {sun = 0, mon = 1, tue = 2, wed = 3, thu = 4, fri = 5, sat = 6}, {first = 0, second = 1, third = 2, fourth = 3, last = 4}
 			local v1, v2 = input:match('(.+)(...)')
 			if W[v1 or ''] and D[v2 or ''] then -- Variable day
 				if v1 == 'last' then
-					local L = 36 + D[v2] - StartDay
-					return L - math.ceil((L - mLength) / 7) * 7
+					local L = 36 + D[v2] - Time.stats.startday
+					return L - math.ceil((L - Time.stats.clength) / 7) * 7
 				else
-					return rotate(D[v2]) + 1 - StartDay + (StartDay > rotate(D[v2]) and 7 or 0) + 7 * W[v1]
+					return rotate(D[v2]) + 1 - Time.stats.startday + (Time.stats.startday > rotate(D[v2]) and 7 or 0) + 7 * W[v1]
 				end
 			else -- Error
 				return ErrMsg(0, 'Invalid Variable {%s} in %s', input, source)
